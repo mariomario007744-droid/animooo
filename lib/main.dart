5fa00885386cf6b58c05efec366a7056985f7138
mario <mariomario007744@gmail.com>
2026-07-24 02:33:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(AnimoooApp());
+}
+
+class AnimoooApp extends StatelessWidget {
+  const AnimoooApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
