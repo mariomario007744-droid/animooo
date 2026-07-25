d0ff42a5bdc20b0dca022abe8426a8cafca4fdd8
mario <mariomario007744@gmail.com>
2026-07-25 05:31:44 +0300

    create login view

--- lib/core/utils/app_routing.dart
+++ lib/core/utils/app_routing.dart
@@ -1,3 +1,4 @@
+import 'package:animooo/features/authentication/presentation/views/login_view.dart';
 import 'package:animooo/features/splash_view/presentation/views/custom_splash_view.dart';
 import 'package:animooo/features/splash_view/presentation/views/simple_splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -12,6 +13,10 @@ abstract class AppRoutes {
       GoRoute(
         path: CustomSplashView.routeName,
         builder: (context, state) => CustomSplashView(),
+      ),
+      GoRoute(
+        path: LoginView.routeName,
+        builder: (context, state) => LoginView(),
       ),
     ],
   );
--- lib/features/splash_view/presentation/views/custom_splash_view.dart
+++ lib/features/splash_view/presentation/views/custom_splash_view.dart
@@ -1,9 +1,29 @@
 import 'package:animooo/core/utils/assets_data.dart';
+import 'package:animooo/features/authentication/presentation/views/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomSplashView extends StatelessWidget {
+class CustomSplashView extends StatefulWidget {
   const CustomSplashView({super.key});
   static const String routeName = '/custom_splash_view';
+
+  @override
+  State<CustomSplashView> createState() => _CustomSplashViewState();
+}
+
+class _CustomSplashViewState extends State<CustomSplashView> {
+  @override
+  void initState() {
+    super.initState();
+    navigation();
+  }
+
+  navigation() async {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => context.go(LoginView.routeName),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
